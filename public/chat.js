047bfbe6f90b791e4f922507a1b5036c23a4a47d
suru03 <107910750+suru03@users.noreply.github.com>
2022-09-14 11:59:46 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-let socket = io.connect("https://calm-lime-gopher-wrap.cyclic.app");
+let socket = io.connect("https://calm-lime-gopher-wrap.cyclic.app/");
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
